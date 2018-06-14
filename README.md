d6b0d5ebc7f4a3fa20a1fdf84b97d69da1b2a8fa
Elliott Hilaire <elliott.hilaire@gmail.com>
2018-06-14 13:09:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+Syncs log files from an S3 bucket and makes a html page with goaccess.
+
+1. Configure .envrc
+
+2. Load with .envrc with direnv or run `source .envrc`
+
+3. Run
+
+```
+./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -ex
+
+aws s3 sync "s3://$S3_BUCKET/" "./logs/$S3_BUCKET/"
+
+find "./logs/$S3_BUCKET/$S3_LOG_PATH" | xargs gunzip -c | goaccess --log-format CLOUDFRONT --output "./output/$S3_BUCKET.html" -
