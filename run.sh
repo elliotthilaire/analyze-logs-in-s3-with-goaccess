c6d07927007248f509441e6e7c71d372243dd3c1
Elliott Hilaire <elliott.hilaire@gmail.com>
2019-01-13 11:50:33 +1100

    Add `-type f` to `find`
    Fixes trying to parse a directory name into gunzip

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -ex
 
 aws s3 sync "s3://$S3_BUCKET/" "./logs/$S3_BUCKET/"
 
-find "./logs/$S3_BUCKET/$S3_LOG_PATH" | xargs gunzip -c | goaccess --log-format CLOUDFRONT --output "./output/$S3_BUCKET.html" -
+find "./logs/$S3_BUCKET/$S3_LOG_PATH" -type f | xargs gunzip -c | goaccess --log-format CLOUDFRONT --output "./output/$S3_BUCKET.html" -
